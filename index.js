02a4d8ef2f2e3243c88feecd0a6d45880536eadf
Patrik Šimunič <patriksimunic@hotmail.com>
2018-02-09 20:41:13 +0100

    Add build script; v1.0.0

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.preserve = require('./lib/preserve.js');
+exports.preserveDeep = require('./lib/preserveDeep.js');
+exports.replace = require('./lib/replace.js');
+exports.replaceDeep = require('./lib/replaceDeep.js');
