46c1fa8f9edd7a790debf99eb56eddaf43bbbffa
Patrik Šimunič <patriksimunic@hotmail.com>
2018-02-11 11:27:24 +0100

    Fix module resolve

--- index.js
+++
@@ -1,4 +0,0 @@
-exports.preserve = require('./lib/preserve.js');
-exports.preserveDeep = require('./lib/preserveDeep.js');
-exports.replace = require('./lib/replace.js');
-exports.replaceDeep = require('./lib/replaceDeep.js');
